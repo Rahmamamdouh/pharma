682e780f3f2e5abee4e0665fda5c99b0c00b4efd
Rahmamamdouh <59338348+Rahmamamdouh@users.noreply.github.com>
2020-03-11 13:13:30 +0200

    Add files via upload

---
+++ database/migrations/2020_03_11_081118_create_carts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCartsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('carts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('medicine_id');
+            $table->integer('cart_quantity');
+            $table->integer('cart_single_price');
+            $table->integer('cart_total_price');
+            $table->integer('user_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('carts');
+    }
+}
