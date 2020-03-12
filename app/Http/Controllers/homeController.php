3ee60364bc62e739807e921c9a2b8a003d5c4ea6
Rahmamamdouh <59338348+Rahmamamdouh@users.noreply.github.com>
2020-03-12 16:28:34 +0200

    Update homeController.php

--- app/Http/Controllers/homeController.php
+++ app/Http/Controllers/homeController.php
@@ -11,8 +11,7 @@ class HomeController extends Controller
      *
      * @return void
      */
-    public function __construct()
-    {
+    public function __construct(){
         $this->middleware('auth');
     }
 
@@ -21,14 +20,12 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function index()
-    {
+    public function index(){
         return view('home');
     }
 
-    public function adminHome()
-    {
+    public function adminHome(){
         return view('adminHome');
-
     }
+
 }
