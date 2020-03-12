bbe2fcb20803792d54141da5d0675ee0c5cbb750
Rahmamamdouh <59338348+Rahmamamdouh@users.noreply.github.com>
2020-03-12 16:28:06 +0200

    Update mainController.php

--- app/Http/Controllers/mainController.php
+++ app/Http/Controllers/mainController.php
@@ -4,16 +4,16 @@ use Illuminate\Http\Request;
 use DB;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
-class mainController extends Controller {
-//public function insertform(){
-//return view('checkout');
-//} 
-public function insert(Request $request){
-$customer_firstname = $request->input('customer_firstname');
-$customer_lastname = $request->input('customer_lastname');
-$customer_phone = $request->input('customer_phone');
-
-$data=array('customer_firstname'=>$customer_firstname,"customer_lastname"=>$customer_lastname,"customer_phone"=>$customer_phone);
-DB::table('customers')->insert($data);
-}
+class mainController extends Controller
+{
+	//public function insertform(){
+	//return view('checkout');
+	//} 
+	public function insert(Request $request){
+	$customer_firstname = $request->input('customer_firstname');
+	$customer_lastname = $request->input('customer_lastname');
+	$customer_phone = $request->input('customer_phone');
+	$data=array('customer_firstname'=>$customer_firstname,"customer_lastname"=>$customer_lastname,"customer_phone"=>$customer_phone);
+	DB::table('customers')->insert($data);
+	}
 }
