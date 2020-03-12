83c6d12c2bf3461cfea45f01a2d17a0d1ce28c43
Rahmamamdouh <59338348+Rahmamamdouh@users.noreply.github.com>
2020-03-12 16:26:17 +0200

    Update LoginController.php

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -1,7 +1,6 @@
 <?php
 
 namespace App\Http\Controllers\Auth;
-
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
@@ -10,90 +9,61 @@ use Illuminate\Support\Facades\Auth;
 use App\Medicine;
 use App\Type;
 use App\Pharmacist;
+use App\Store;
 use DB;
 //Auth::logout();
+
 class LoginController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Login Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles authenticating users for the application and
-    | redirecting them to your home screen. The controller uses a trait
-    | to conveniently provide its functionality to your applications.
-    |
-    */
+  /*
+  |--------------------------------------------------------------------------
+  | Login Controller
+  |--------------------------------------------------------------------------
+  |
+  | This controller handles authenticating users for the application and
+  | redirecting them to your home screen. The controller uses a trait
+  | to conveniently provide its functionality to your applications.
+  |
+  */
 
-    use AuthenticatesUsers;
+  use AuthenticatesUsers;
 
-    /**
-     * Where to redirect users after login.
-     *
-     * @var string
-     */
-   //protected $redirectTo = RouteServiceProvider::HOME;
-      protected function redirectTo()
-      {
-          if(Auth::user()->usertype == 'admin')
-          {
-            return 'MedicineTable.addNewMedicine';  
-          }
-          else
-          {
-             return 'pharmaIndex';   
-          }
+  /**
+   * Where to redirect users after login.
+   *
+   * @var string
+   */
+  //protected $redirectTo = RouteServiceProvider::HOME;
+
+  /**
+   * Create a new controller instance.
+   *
+   * @return void
+   */
+
+  public function __construct(){
+      $this->middleware('guest')->except('logout');
+  }
+
+  public function login(Request $request){
+    $input=$request->all();
+    $this->validate($request, [
+      'email'=>'required|email',
+      'password'=>'required',
+    ]);
+
+    //actors login:
+    //actors:user, pharmacist and manager
+    if (auth()->attempt(array('email'=>$input['email'], 'password'=>$input['password']))){
+      if (auth()->user()->usertype=='admin'){
+        return redirect('/allPharmacists');
       }
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('guest')->except('logout');
-    }
-    public function login(Request $request)
-    {
-        $input = $request->all();
-
-        $this->validate($request, [
-            'email' => 'required|email',
-            'password' => 'required',
-        ]);
-        //user&admin&pharmacist login:
-        if (auth()->attempt(array('email' => $input['email'], 'password' => $input['password']))) {
-            if (auth()->user()->usertype == 'admin') {
-              // return redirect()->route('\newMedicine');
-              // return redirect(MedicineTable.addNewMedicine);
-             // return 'MedicineTable.addNewMedicine'; 
-        $pharmacists=DB::table('pharmacists')->paginate(10);
-            return view('PharmacistTable.viewAllPharmacists',compact('pharmacists'));
-        
-             }elseif (auth()->user()->usertype == 'pharmacist'){
-                 $types=Type::all();
-                 
-             return view('MedicineTable.addNewMedicine',compact('types'));
-            } else {
-               // return redirect()->route('index');
-                //DONE
-                //return redirect('index');
-                //return 'pharmaIndex';
-                $medicines=Medicine::all();
-             return view('pharmaIndex',compact('medicines'));
-                    
-            
-          // } else {
-          // return redirect()->route('login')
-           //   ->with('error', 'Email-Address And Password Are Wrong.');
-        //}
-        
-
-                    }
-    
-    
-
-        }   
+      elseif (auth()->user()->usertype=='pharmacist'){
+        return redirect('/allMedicines');
+      } 
+      else{
+        return redirect('/store');
+      }
+    }   
+  }
 }
-}
-
